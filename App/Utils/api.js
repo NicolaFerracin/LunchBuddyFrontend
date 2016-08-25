175914aeee0828917c53f360c33b62c3adcb716b
Nicola Ferracin <nicola.ferracin@gmail.com>
2016-08-25 13:27:04 +0200

    no message

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+'use strict';
+
 const events = [
   {name: 'Burger restaurant', peopleAttending: 3, image: 'http://www.bk.com/sites/default/files/New_BaconCheeseburgerDeluxe_thumb.png', rating: 4, price: 3, distance: 10, time: '11:30'}
 ]
@@ -6,37 +7,6 @@ const events = [
 const locations = [
   {name: 'Burger restaurant', peopleAttending: 3, image: 'http://www.bk.com/sites/default/files/New_BaconCheeseburgerDeluxe_thumb.png', rating: 4, price: 3, distance: 10, time: '11:30'}
 ]
-
-var api = {
-  getEvents() {
-    // var url = `https://api.github.com/users/${username}`;
-    // return fetch(url).then((res) => res.json());
-    return Promise.resolve(events)
-  },
-  getLocations() {
-    return Promise.resolve(locations)
-  }
-  getRepos(username) {
-    username = username.toLowerCase().trim();
-    var url = `https://api.github.com/users/${username}/repos`;
-    return fetch(url).then((res) => res.json());
-  },
-  getNotes(username) {
-    username = username.toLowerCase().trim();
-    var url = `https://github-notes2.firebaseio.com/${username}.json`;
-    return fetch(url).then((res) => res.json);
-  },
-  createEvent(event) {
-    username = username.toLowerCase().trim();
-    var url = `https://github-notes2.firebaseio.com/${username}.json`;
-    return fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(note)
-    }).then((res) => res.json())
-  }
-};
-=======
-'use strict';
 
 var api = {
     createEvent(timestamp, locationId) {
@@ -68,7 +38,7 @@ var api = {
         return [
             {
                 email: 'johndoe@gmail.com',
-                name: 'John Doe'     
+                name: 'John Doe'
             },
             {
                 email: 'maxmusterman@gmail.com',
@@ -94,6 +64,5 @@ var api = {
         return fetch(url, { method: 'get' }).then((res) => res.json());
     }
 }
->>>>>>> d6a9fa9a50f49f4950b04489934289e82c7a8a99
 
 module.exports = api;
